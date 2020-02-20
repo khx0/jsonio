1b9edcef712fe5e9688278a5112798b5ca592432
Nikolas Schnellbächer <khx0@posteo.net>
2020-02-20 22:50:29 +0100

    added test_jsonio.py

---
+++ test_jsonio.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+##########################################################################################
+# author: Nikolas Schnellbaecher
+# contact: khx0@posteo.net
+# date: 2020-02-20
+# file: test_jsonio.py
+##########################################################################################
+
+import sys
+import time
+import datetime
+import os
+import math
+import numpy as np
+
+today = datetime.datetime.now().strftime("%Y-%m-%d")
+
+BASEDIR = os.path.dirname(os.path.abspath(__file__))
+RAWDIR = os.path.join(BASEDIR, 'raw')
+OUTDIR = os.path.join(BASEDIR, 'out')
+ 
+if __name__ == '__main__':
+    
+    pass
+
